132e35aafa97e33fad485a1d9cb2a5a87e970160
Simon McCartney <simon.mccartney@hp.com>
2016-01-13 17:04:01 +0000

    update VPN endpoint name

--- salt/states/hpcs_vpn/hpcs_auto.sh
+++ salt/states/hpcs_vpn/hpcs_auto.sh
@@ -3,4 +3,4 @@
 SCRIPTPATH=$(dirname $(readlink -f $0))
 
 
-(cd $SCRIPTPATH ; python $SCRIPTPATH/juniperncprompt_auto.py --nc-path $SCRIPTPATH --cert $SCRIPTPATH/ssl-uswest.crt --login-path /dana-na/auth/url_1/login.cgi --realm 2FA remote.aw2.hpcloud.net )
+(cd $SCRIPTPATH ; python $SCRIPTPATH/juniperncprompt_auto.py --nc-path $SCRIPTPATH --cert $SCRIPTPATH/ssl-uswest.crt --login-path /dana-na/auth/url_1/login.cgi --realm 2FA uswest.vpn.hpcloud.net )
